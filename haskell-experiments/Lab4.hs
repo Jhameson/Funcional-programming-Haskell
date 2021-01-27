550c7a24026d1c6a471e95333f8ab27547dba918
Jhameson <jhamesonlucas22@gmail.com>
2021-01-26 23:05:01 -0300

    att

--- haskell-experiments/Lab4.hs
+++ haskell-experiments/Lab4.hs
@@ -5,27 +5,27 @@ import Functions
 import List
 import qualified BSTree as BST
 
+-----------------------------------------------------
 data BSTree k v = Empty
                 | Branch (k, v) (BSTree k v) (BSTree k v)
 
-
-main :: IO ()
-main = readFile " heyjude.txt " >>= putStrLn
-
 empty :: BSTree k v
 empty = Empty
+------------------------------------------------------
 
 --main :: IO ()
 --main = readFile " heyjude . txt " >>= pure . process >>= putStrLn
+main :: IO ()
+main = readFile " heyjude.txt " >>= putStrLn
+
+
+-------------------------------------------------------
 
 --Questão 01
 my_remove :: Eq a => a -> [a] -> [a]
 my_remove x = filter (/=x)
 
 --Questão 02
---split' :: Eq a => a -> [a] -> [[a]]
---split' a [] = []
---split' a xs = x : split' a (drop 1 y) where (x,y) = span (/= a) xs
 
 split' :: Eq a => a -> [a] -> [[a]]
 split' _ [] = []
@@ -40,18 +40,41 @@ lines' = split '\n'
 
 words' :: [Char] -> [[Char]]
 words' = split ' '
---Questão 04
-count :: [[Char]] -> BST.BSTree [Char] Int
-count xs = foldr BSTree. 
 
+--Questão 04
+count :: [[Char]] ->BST.BSTree [Char] Int
+count xs = foldl f BST.empty xs where 
+	f btree plv= if (BST.contains btree plv)
+		then BST.update btree plv (+1)                                                                       
+		else BST.insert btree (plv,1)
 --Questão 05
+
+process :: [Char] -> [Char]
+process = makeOutput . countWords . clean where
+    clean = undefined
+    countWords = undefined
+    makeOutput = concat . intercalate " \n" . map f . BST.inOrder
+    f ( str , c ) = str ++ ": " ++ show c
+
+countWords :: [String] -> BST.BSTree [String] Int
+countWords xs = foldl f BST.empty xs where 
+	f btree plv= if (BST.contains btree plv)
+		then BST.update btree plv (+1)                                                                       
+		else BST.insert btree (plv,1)
 
 clean :: [Char] -> [Char]
 clean xs = foldr my_remove xs [',','!','?',')','(']
 
---countWords :: String -> BST.BSTree
+
+
 
 --Questão 06
+--
+--
+--
+--
+--
+---------------------------------------------------------------------------------------------
 --E makeOutput está esperando uma BSTree
 -- Lembra que countWords vai receber o conteúdo do arquivo 
 --(limpo) e vai só contar as palavras numa BSTree String Int
@@ -61,9 +84,32 @@ clean xs = foldr my_remove xs [',','!','?',')','(']
 
 --Depois disso um foldr com algumas operações de BSTree dá conta de 
 --construir a BSTree que associa a cada palavra o número de ocorrências dela
+----------------------------------------------------------------------------------------------
+--
+--
+--
+--
+-- exercicio 13
+--breadth :: BSTree k v -> [[k,v]]
 
--- exercicio
+-- exercicio 14
+
+leaves :: BSTree k v -> Int
+leaves  Empty = 0
+leaves (Branch a Empty Empty) = 1
+leaves (Branch a left  right) = leaves left + leaves right + 0
+
+
+-- exercicio 15
 inOrder :: BSTree k v -> [(k, v)]
 inOrder Empty = []
-inOrder (Branch keyval lt rt) = inOrder lt ++ (keyval : inOrder rt)
+inOrder (Branch n esq dir) = inOrder esq ++ (n : inOrder dir)
+
+preOrder :: BSTree k v -> [(k, v)]
+preOrder  Empty = []
+preOrder  (Branch n esq dir) = n:preOrder  esq ++ preOrder  dir
+
+postOrder::BSTree k v -> [(k, v)]
+postOrder Empty = []
+postOrder (Branch n esq dir) = postOrder esq ++ postOrder dir ++ [n]
 
