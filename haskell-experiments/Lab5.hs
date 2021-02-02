ee1cc15907599831592cc3d6e38e396b0b1f00ec
Jhameson <jhamesonlucas22@gmail.com>
2021-02-02 15:25:41 -0300

    att

--- haskell-experiments/Lab5.hs
+++ haskell-experiments/Lab5.hs
@@ -1,14 +1,21 @@
+{-
+	Todas as funções estão contidas no Lab4, as únicas novas inserções foram
+	a função "myCompare", "(!?)" e uma alteração na "process" (a exigida do Lab5)
+-}
+
+
+
 module Main where
 
-import Prelude (Eq, (==), (/=), Int, (+), (>),
- (<),(<=), Char, IO, readFile, putStrLn, 
- show,(>>=), pure, Ord {-, Ordering(LT, EQ, GT), otherwise-})
+import Prelude (Eq, Bool(True, False), Int, Char, IO, Ord , (==), (/=),  (+),  (-), (>=),
+   readFile, putStrLn, 
+ show,(>>=), pure,   otherwise)
 
 
 
 import Maybe
 import Functions
-import List ((++), intercalate, concat, foldl, span, dropWhile, filter, {-length,-} sortBy)
+import List ((++), intercalate, concat, foldl, span, dropWhile, filter , sortBy)
 
 
 import qualified BSTree as BST
@@ -35,52 +42,58 @@ count' = foldl f BST.empty where
   lkp = BST.lookup bst word
   g Nothing = BST.insert bst (word, 1)
   g (Just _) = BST.update bst word (+1)
-------------------------------
+
+{-
+	#########################################################	
+-}
+-- LABORATÓRIO 06
  
 process' :: [Char] -> [Char]
-process' = makeOutput . countWords . clean where
+process' =  makeOutput .  countWords . clean where
  clean = remove' '.' . remove' '!' . remove' '?' . remove' '(' . remove' ')' . remove' ','
  countWords = count' . concat . map words' . lines'
- makeOutput = concat . intercalate "\n" . map f . BST.inOrder
- f (word, count') = word ++ ": " ++ show count'
+ makeOutput = concat . intercalate "\n" .  map f . sortBy myCompare . BST.inOrder
+ f (word, count') = word ++ ": " ++ show  count'
 
---------------------------------------
---------------------------------------
-merge :: Ord a => [a] -> [a] -> [a]
-merge [] x = x
-merge x [] = x
-merge (x:xs) (y:ys) = if (x <= y) then x : merge xs (y:ys) else y : merge (x:xs) ys
 
-my_split :: [a] -> ([a], [a])
-my_split [] = ([],[])
-my_split [a] = ([a],[])
-my_split (x:xs:xss) = (x:d,xs:j) where (d,j) = my_split xss
+myCompare :: (Ord a, Ord b) => (a,b) -> (a,b) -> Bool
+myCompare (_,v1) (_,v2) | v1 >= v2 = True | otherwise = False
 
 {-
-mergesort :: (Ord a , Ord b) => (a,b) -> (a,b)
-mergesort [] = []
-mergesort xs = if (t<2) then xs else merge m n 
- where 
-  t = length xs 
-  (a,b) = my_split xs 
-  m = mergesort a 
-  n = mergesort b
- -}
+	#########################################################	
+-}
+
 --------------------------------------
---------------------------------------
+--Exercicio 16
+(!?) :: [a] -> Int -> Maybe a
+[] !? _ = Nothing
+(x: _) !? 0 = Just x
+(_: xs ) !? k = xs !? (k - 1)
+
+-------------------------------------
+
+
+-- uma_lista = [("dois", 2),("um", 1),("tres", 3)]
+--ordena lista = sortBy my_compare lista
+
+
+------------------------------
+--valor (_, n) = n
 {-
-uma_lista = [("dois", 2),("um", 1),("tres", 3)]
+mis :: Ord a  => [a] -> [a] -> [a]
+mis [] y = y
+mis x [] = x
+mis (x:xs) (y:ys) = if (xs <= ys) 
+ then x: mis xs (y:ys) 
+ else y: mis (x:xs) ys
 -}
 
-{-}
-compara :: (Ord a, Ord b) => (a,b) -> (a,b) -> Ordering
-compara (a1,b1) (a2,b2) | b1 > b2 =GT | b1 == b2 = EQ  | otherwise = LT
--}
-
---ordena lista = sortBy compara lista
-
-
---------------------------------------
---------------------------------------
 main :: IO ()
-main = readFile "heyjude.txt" >>=   pure  . sortBy (<). process' >>= putStrLn
+main = readFile "heyjude.txt" >>=    pure  .   process' >>= putStrLn
+
+
+
+
+
+
+
