949d2d6c1119ffb85085260ac64d26f20c4b0ba7
Jhameson <jhamesonlucas22@gmail.com>
2021-03-18 16:03:17 -0300

    att

---
+++ haskell-experiments/Lab10.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Prelude (Ord, (<=), (>=), (-), Bool, IO, putStrLn, (<$>), pure, (<*>), undefined)
+import Bool
+import Functions
+import List
+import Maybe
+
+maximum :: Ord a => [a] -> Maybe a
+maximum [] = Nothing
+maximum a = undefined 
+
+main :: IO()
+main = undefined
